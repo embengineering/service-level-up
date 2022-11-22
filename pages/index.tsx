3ad1bc0d43cd3ba7dc3cc43ed615049d6eeecea6
Emmanuel Morales <emorales0024@gmail.com>
2022-11-22 22:04:58 +0000

    Add MUI, Firebase, and Notistack

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,43 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, GoogleAuthProvider, getRedirectResult, User } from "firebase/auth";
+import { firebaseConfig } from './config';
+import { useEffect, useState } from 'react';
+import { useSnackbar } from 'notistack';
 
 export default function Home() {
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [user, setUser] = useState<User>();
+
+  const retrieveAuthAsync = async () => {
+    if (user) return;
+
+    try {
+      const result = await getRedirectResult(auth);
+
+      console.log(result);
+
+      if (!result) {
+        return;
+      }
+
+      setUser(result.user);
+
+    } catch (err) {
+      enqueueSnackbar('Error attempting to authenticate with Google. Try again.');
+    }
+  }
+
+  useEffect(() => {
+    retrieveAuthAsync();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
